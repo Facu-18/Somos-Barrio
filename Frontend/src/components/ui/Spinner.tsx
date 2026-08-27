6d2154e1151651ddb6a41dc6e499e19a0aabb56c
Facu-18 <facundomoriconi19@gmail.com>
2026-08-27 15:02:48 -0300

    eliminar frontend

--- Frontend/src/components/ui/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from '../../lib/utils'
-
-export function Spinner({ className }: { className?: string }) {
-  return (
-    <div className={cn('flex justify-center items-center py-12', className)}>
-      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-green-700" />
-    </div>
-  )
-}
